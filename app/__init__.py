2e13293556bdf0977ec2b5563d6555c9f37b289c
EmilieYZhang <emilieyzh@gmail.com>
2022-08-06 18:51:17 -0400

    Updated flask routing with new pages recently added

--- app/__init__.py
+++ app/__init__.py
@@ -17,15 +17,27 @@ def index():
     """
     return render_template('index.html', title="HackTheMap", url=os.getenv("URL"))
 
-# Route for the profile page
+# Route for the map page
 @app.route('/map')
 def map():
-    """
-    Loads profile dynamically from the JSON file and serves profile page.
-    
-    If profile could not be found, redirects to the landing page.
-    """
     return render_template('map.html', url=os.getenv("URL"), API_KEY=os.getenv("API_KEY"))
+
+
+# Route for the account page
+@app.route('/account')
+def account():
+    return render_template('account.html', url=os.getenv("URL"))
+
+# Route for the contact page
+@app.route('/contact')
+def contact():
+    return render_template('contactus.html', url=os.getenv("URL"))
+
+
+# Route for the goal page
+@app.route('/goal')
+def goal():
+    return render_template('goals.html', url=os.getenv("URL"))
 
 # Route for handling 404 errors
 @app.errorhandler(404)
