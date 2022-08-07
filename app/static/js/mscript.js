a4e09947e3fc483f98990414c428f973cc416f86
EmilieYZhang <emilieyzh@gmail.com>
2022-08-06 23:14:55 -0400

    Debugging with mentor to fix map markers inconsistency issue, also added info box feature

--- app/static/js/mscript.js
+++ app/static/js/mscript.js
@@ -1,15 +1,28 @@
-function Mapping() {
+function initMap() {
     var mapProp = { // map view begins zoomed out
       center: new google.maps.LatLng(0, 0),
       zoom: 3,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    for (let i = 0; i < countries.length; i++) { 
-      var marker = new google.maps.Marker({ // add a marker for each country visited
-          position: new google.maps.LatLng(countries[i].lat, countries[i].long),
-          map: map
-      });
-      addInfoWindow(marker, "content");
+    for (let i = 0; i < places.length; i++) {
+        geocode(places[i], ({ lat, long }) => {
+            var marker = new google.maps.Marker({ // add a marker for each country visited
+                position: new google.maps.LatLng(lat, long),
+                map: map
+            });
+            const content = '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<h1 id="firstHeading" class="firstHeading">'+ countries[i].event +'</h1>' +
+            '<div id="bodyContent">' +
+            "<h4>Location: " + countries[i].location +'</h4>' +
+            "<h4>Date: " + countries[i].date +'</h4>' +
+            '<h4>More information: <a href="'+ countries[i].link + '">Take me to their website!</a>' +
+            "</h4>" +
+            "</div>" +
+            "</div>"
+            addInfoWindow(marker, content);
+        })
     }
 
     function addInfoWindow(marker, message) {
@@ -21,4 +34,29 @@ function Mapping() {
             infoWindow.open(map, marker);
         });
     }
+}
+
+// find long and lat coordinates of locations
+function geocode(place, callback){
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+        params:{
+            address: place,
+            key: API_KEY,
+        }
+    })
+    .then(function(response){
+        // Log full response
+        console.log(response);
+
+        // long and lat
+        var lati = response.data.results[0].geometry.location.lat;
+        var longit = response.data.results[0].geometry.location.lng;
+        callback({
+            lat: lati,
+            long: longit
+        });
+    })
+    .catch(function(error){
+        console.log(error);
+    });
 }
